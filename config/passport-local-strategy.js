81ea2e38f4aad9db14700c18c83a0592c9b66916
vivek <solankivivek01@gmail.com>
2021-03-01 10:27:39 -0800

    passport  authentication(CORRECT)

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,36 +1,54 @@
-const passport=require('passport');
-const LocalStrategy=require('passport-local').Strategy;
-const User=require('../models/user');
-//authentication user passport
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-},
-function(email,password,done){
-// find a user and establish the identity
-User.findOne({email: email},(err,user)=>{
-    if(err){
-        console.log('error if finding user-----> passport');
-        return done(err);
-    }
-    if(!user||user.password !=password){
-        console.log('Invalid user name or password');
-        return done(null,false);
-    }
-    return done(null,user);
-});
-}
-));
-//serialllize the user to decide which key is to be kept in cookies
-passport.serializeUser((user,done)=>{done(null,user.id);});
+const passport = require('passport');
 
-//deseriallizing the user from the key in the
-passport.deserializeUser((id,done)=>{
-    User.findById(id,(err,user)=>{
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+
+// authentication using passport
+passport.use(new LocalStrategy({
+        usernameField: 'email'
+    },
+    function(email, password, done){
+        // find a user and establish the identity
+        User.findOne({email: email}, function(err, user)  {
+            if (err){
+                console.log('Error in finding user --> Passport');
+                return done(err);
+            }
+
+            if (!user || user.password != password){
+                console.log('Invalid Username/Password');
+                return done(null, false);
+            }
+
+            return done(null, user);
+        });
+    }
+
+
+));
+
+
+// serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
         if(err){
-            console.log('error if finding user-----> passport');
+            console.log('Error in finding user --> Passport');
             return done(err);
         }
-        return done(null,user);
+
+        return done(null, user);
+    });
 });
-});
-module.exports=passport;
+
+
+
+module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const { Passport } = require('passport');
 const router = express.Router();
-// const passport=require('passport');
+const passport = require('passport');
 
 const usersController = require('../controllers/users_controller');
 
@@ -10,11 +9,13 @@ router.get('/profile', usersController.profile);
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
-router.post('/create-session',Passport.authenticate(
-    'local',
-    {failureRedirect:'/user/sign-in'},
-),usersController.createSession);
+
 router.post('/create', usersController.create);
 
+// use passport as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
 
 module.exports = router;
