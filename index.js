3f29a5ccc4952f2860bb83ab95c5dfcbab812177
vivek <solankivivek01@gmail.com>
2021-03-02 00:45:50 -0800

    improving

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
-
+const MongoStore=require('connect-mongo',session);
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -27,7 +27,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+//mongo store is used to store the session cookies inn the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -36,7 +36,18 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
+    },
+    store: new MongoStore(
+        {
+        
+            mongooseConnection: db,
+            autoRemove: 'disable'
+        
+    },
+    function(err){
+        console.log(err||'mogodb setup done');
     }
+    )
 }));
 
 app.use(passport.initialize());
