e6976b52e23d64c309d999219bd29150d15138f8
vivek <solankivivek01@gmail.com>
2021-03-03 18:44:43 +0530

    added delete button to comment and post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
-const Comment=require('../models/comment');
+const Comment = require('../models/comment');
+
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
@@ -10,18 +11,20 @@ module.exports.create = function(req, res){
         return res.redirect('back');
     });
 }
-module.exports.destroy=(req,res)=>{
-    Post.findById(req.params.id,(err,post)=>{
-        //checking if one want to delete post is his/her own or not
-        //.id means converting the object id into string
-        if(post.user==req.user.id){
+
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id, function(err, post){
+        // .id means converting the object id into string
+        if (post.user == req.user.id){
             post.remove();
 
-            Comment.deleteMany({post: req.params.id},(err)=>{
-                res.redirect('back');
+            Comment.deleteMany({post: req.params.id}, function(err){
+                return res.redirect('back');
             });
         }else{
             return res.redirect('back');
         }
+
     });
 }
--- models/comment.js
+++ models/comment.js
@@ -20,4 +20,4 @@ const commentSchema = new mongoose.Schema({
 });
 
 const Comment = mongoose.model('Comment', commentSchema);
-module.exports = Comment;
+module.exports = Comment; 
