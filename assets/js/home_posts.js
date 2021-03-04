472ae55c931efcd12fef3d23be85b4348362d44a
vivek <solankivivek01@gmail.com>
2021-03-04 19:03:19 +0530

    added Ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,56 +1,60 @@
-{
-    let createPost=()=>{
-        let newPostForm=$('#new-post-form');
+{   
+    // method to submit the form data for new post using AJAX
+    let createPost = function(){
+        let newPostForm = $('#new-post-form');
 
-        newPostForm.submit((e)=>{
+        newPostForm.submit(function(e){
             e.preventDefault();
+
             $.ajax({
                 type: 'post',
                 url: '/posts/create',
                 data: newPostForm.serialize(),
-                success: (data)=>{
-              let newPost=new newPostDom(data.data.post);
-              $('#posts-list-container>ul').prepend(newPost);
-                },
-                error: (error)=>{
+                success: function(data){
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
+                }, error: function(error){
                     console.log(error.responseText);
                 }
-            })
+            });
         });
     }
-    //create A DOM for new post
-    let newPostDom=function(post){
+
+
+    // method to create a post in DOM
+    let newPostDom = function(post){
         return $(`<li id="post-${post._id}">
-        <p>
-            
-            <small>
-                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
-            </small>
-            <% } %>
-            ${ post.content}
-            <br>
-            <small>
-                ${ post.user.name }
-            </small>
-        </p>
-        <div class="post-comments">
-           
-                <form action="/comments/create" id="new-post-form" method="POST">
-                    <input type="text" name="content" placeholder="Type Here to add comment..." required>
-                    <input type="hidden" name="post" value="${post._id}" >
-                    <input type="submit" value="Add Comment">
-                </form>
-
-           
-
-            <div class="post-comments-list">
-                <ul id="post-comments-${post._id}">
+                    <p>
+                        
+                        <small>
+                            <a class="delete-post-button"  href="/posts/destroy/${ post.id }">X</a>
+                        </small>
+                       
+                        ${ post.content }
+                        <br>
+                        <small>
+                        ${ post.user.name }
+                        </small>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${ post._id }" >
+                                <input type="submit" value="Add Comment">
+                            </form>
+               
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${ post._id }">
+                                
+                            </ul>
+                        </div>
+                    </div>
                     
-                </ul>
-            </div>
-        </div>
-        
-    </li>`)
+                </li>`)
     }
-createPost();
+
+
+    createPost();
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,26 +3,26 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try{
-        let post=await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
-        if(req.xhr){
+        
+        if (req.xhr){
             return res.status(200).json({
                 data: {
-                    post: post,
+                    post: post
                 },
                 message: "Post created!"
-            })
+            });
         }
-        req.flash('success','Post publish !');
+
+        req.flash('success', 'Post published!');
         return res.redirect('back');
 
     }catch(err){
-
         req.flash('error', err);
         return res.redirect('back');
-
     }
   
 }
@@ -37,18 +37,18 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
-            req.flash('success','Post and Associated comments deleted !');
+
+            req.flash('success', 'Post and associated comments deleted!');
+
             return res.redirect('back');
         }else{
-            req.flash('error','you cannot delete this post');
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
 
     }catch(err){
-      
         req.flash('error', err);
         return res.redirect('back');
-        
     }
     
 }
