de99f30676371703b3b2636afef21c5645986a08
vivek <solankivivek01@gmail.com>
2021-03-04 18:54:28 +0530

    adding post(AJAX)

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -9,13 +9,48 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: (data)=>{
-                console.log(data);
+              let newPost=new newPostDom(data.data.post);
+              $('#posts-list-container>ul').prepend(newPost);
                 },
                 error: (error)=>{
                     console.log(error.responseText);
                 }
             })
         });
+    }
+    //create A DOM for new post
+    let newPostDom=function(post){
+        return $(`<li id="post-${post._id}">
+        <p>
+            
+            <small>
+                <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+            </small>
+            <% } %>
+            ${ post.content}
+            <br>
+            <small>
+                ${ post.user.name }
+            </small>
+        </p>
+        <div class="post-comments">
+           
+                <form action="/comments/create" id="new-post-form" method="POST">
+                    <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                    <input type="hidden" name="post" value="${post._id}" >
+                    <input type="submit" value="Add Comment">
+                </form>
+
+           
+
+            <div class="post-comments-list">
+                <ul id="post-comments-${post._id}">
+                    
+                </ul>
+            </div>
+        </div>
+        
+    </li>`)
     }
 createPost();
 }
