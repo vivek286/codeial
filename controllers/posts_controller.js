d4c8172ba3eb8c5a5d5052e0de15c3282a9a073f
vivek <solankivivek01@gmail.com>
2021-03-04 15:46:05 +0530

    added notifications to post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,12 +7,14 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
-    
+        req.flash('success','Post publish !');
         return res.redirect('back');
 
     }catch(err){
-        console.log('Error', err);
-        return;
+
+        req.flash('error', err);
+        return res.redirect('back');
+
     }
   
 }
@@ -27,14 +29,17 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
+            req.flash('success','Post and Associated comments deleted !');
             return res.redirect('back');
         }else{
+            req.flash('error','you cannot delete this post');
             return res.redirect('back');
         }
 
     }catch(err){
-        console.log('Error', err);
-        return;
+      
+        req.flash('error', err);
+        return res.redirect('back');
     }
     
 }
