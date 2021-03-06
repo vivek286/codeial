62c33d0872a18535af1875135f5aae371ed59411
vivek <solankivivek01@gmail.com>
2021-03-06 15:45:53 +0530

    uploading avatar image

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -8,18 +8,18 @@ const User = require('../models/user');
 // authentication using passport
 passport.use(new LocalStrategy({
         usernameField: 'email',
-        passReqToCallback: true,
+        passReqToCallback: true
     },
-    function(req , email, password, done){
+    function(req, email, password, done){
         // find a user and establish the identity
         User.findOne({email: email}, function(err, user)  {
             if (err){
-                req.flash('error',err);
+                req.flash('error', err);
                 return done(err);
             }
 
             if (!user || user.password != password){
-              req.flash('error','Invalid Username/Password');
+                req.flash('error', 'Invalid Username/Password');
                 return done(null, false);
             }
 
