e8edd3fc38cd5ab13709c1b76faeee44166d03ca
vivek <solankivivek01@gmail.com>
2021-03-07 00:44:27 +0530

    practise

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,28 @@
+const passport =require('passport');
+const JWTStrategy=require('passport-jwt').Strategy;
+const ExtractJWt=require('passport-jwt').ExtractJwt;
+const User=require('../models/user');
+let opts={
+    jwtFromRequest:ExtractJWt.fromAuthHeaderAsBearerToken,
+    secretOrKey:'codeial'
+}
+
+
+
+
+
+
+passport.use(new JWTStrategy(opts,(jwtPayLoad,done)=>{
+    User.findById(jwtPayLoad._id,(err,user)=>{
+        if(err){
+            console.log('Error in finding user from jwt');
+            return;
+        }
+        if(user){
+            return done(null,user);
+        }else{
+            return done(null,false);
+        }
+    })
+}));
+module.exports=passport;
