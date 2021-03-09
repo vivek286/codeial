af63f14cb52d3f52bb086a0948400afc06723114
vivek <solankivivek01@gmail.com>
2021-03-09 23:49:35 +0530

    adding tokens and jwt

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,16 +1,18 @@
-const Post=require('../../../models/post');
-const Comment=require('../../../models/comment');
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 module.exports.index = async function(req, res){
 
+
     let posts = await Post.find({})
-    .sort('-createdAt')
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    });
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.json(200, {
         message: "List of posts",
         posts: posts
@@ -23,36 +25,25 @@ module.exports.destroy = async function(req, res){
     try{
         let post = await Post.findById(req.params.id);
 
-     //   if (post.user == req.user.id){
+        // if (post.user == req.user.id){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
 
 
-            // if (req.xhr){
-            //     return res.status(200).json({
-            //         data: {
-            //             post_id: req.params.id
-            //         },
-            //         message: "Post deleted"
-            //     });
-            // }
-
-          
-
-            return res.json(200,{
-                message: "post and associated comments deleted sucessfully"
+    
+            return res.json(200, {
+                message: "Post and associated comments deleted successfully!"
             });
         // }else{
         //     req.flash('error', 'You cannot delete this post!');
         //     return res.redirect('back');
-        //  }
+        // }
 
     }catch(err){
-       // req.flash('error', err);
-       console.log('********',err);
-        return res.json(500,{
-            message:"internal server error"
+        console.log('********', err);
+        return res.json(500, {
+            message: "Internal Server Error"
         });
     }
     
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -5,7 +5,8 @@ const postsApi = require("../../../controllers/api/v1/posts_api");
 
 
 router.get('/', postsApi.index);
-router.delete('/:id',postsApi.destroy);
+router.delete('/:id', postsApi.destroy);
+
 
 
 module.exports = router;
