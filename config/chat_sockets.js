27247d4edc2421983501e033ff055554de69b495
vivek286 <solankivivek01@gmail.com>
2021-03-20 22:52:15 +0530

    Chat engine done

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -7,13 +7,16 @@ module.exports.chatSockets = function(socketServer){
 
         socket.on('disconnect', function(){
             console.log('socket disconnected!');
-        });
+         });
 
             socket.on('join_room',function(data){
                 console.log('joining request rec',data);
                 socket.join(data.chatroom);
                 io.in(data.chatroom).emit('user_joined',data);
             });
+                socket.on('send_message',function(data){
+                    io.in(data.chatroom).emit('receive_message',data);
+                })
     });
 
 }
